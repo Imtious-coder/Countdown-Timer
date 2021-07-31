d7d4dd03ed0f01ae60d140d823a7bb3ad7a81109
unknown <iarafat386@gmail.com>
2021-07-31 22:15:35 +0600

    UI, Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Countdown-Timer
 
 #### Preview(https://imtious-coder.github.io/Countdown-Timer/)
+
+## This is Vanila JavaScript project. It's a front end based countdown app.
+
+* Responsive UI.
+* Clean code.
+* Countdown Functionality.
+
+#### Tech and Tools: JavaScript, ES6+, HTML5, CSS3 and Git
+
+###### Imtious Islam
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// DOM
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
+// Static date for countdown...
 const newYears = '31 December 2021'
 
+// Maths
 function countdown() {
     const newYearDate = new Date(newYears);
     const currentDate = new Date();
@@ -14,20 +17,16 @@ function countdown() {
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
-
-
+    // UI data adding...
     daysEl.innerHTML = days
     hoursEl.innerHTML = formateTime(hours)
     minsEl.innerHTML = formateTime(minutes)
     secondsEl.innerHTML = formateTime(seconds)
-
-
 }
-
+// Condition for making "0" to "00" formate..
 function formateTime(time) {
     return time < 10 ? (`0${time}`) : time
 }
-
 countdown()
-
+// Calling time...
 setInterval(countdown, 1000);
